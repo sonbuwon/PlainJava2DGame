84ab739c9e29f1346a2fe62886dd7cf1784d6d3d
BUWON\bwgh0 <bwgh0268@naver.com>
2025-10-29 21:28:01 +0900

    add player collision detection

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -25,6 +26,12 @@ public class Player extends Entity {
 		
 		screenX = gp.screenWidth/2 - (gp.tileSize/2);
 		screenY = gp.screenHeight/2 - (gp.tileSize/2);
+		
+		solidArea = new Rectangle();
+		solidArea.x = 8;
+		solidArea.y = 16;
+		solidArea.width = 32;
+		solidArea.height = 32;		
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -56,19 +63,29 @@ public class Player extends Entity {
 		if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
 			if (keyH.upPressed == true) {
 				direction = "up";
-				worldY -= speed;
 			}
 			else if (keyH.downPressed == true) {
 				direction = "down";
-				worldY += speed;
 			}
 			else if (keyH.leftPressed == true) {
 				direction = "left";
-				worldX -= speed;
 			}
 			else if (keyH.rightPressed == true) {
 				direction = "right";
-				worldX += speed;
+			}
+			
+			// CHECK TILE COLLISION
+			collisionOn = false;
+			gp.cChecker.checkTile(this);
+			
+			// IF COLLISION IS FALSE, PLAYER CAN MOVE
+			if(collisionOn == false) {
+				switch(direction) {
+				case "up": worldY -= speed; break;
+				case "down": worldY += speed; break;
+				case "left": worldX -= speed; break;
+				case "right": worldX += speed; break;
+				}
 			}
 			
 			spriteCounter++;
